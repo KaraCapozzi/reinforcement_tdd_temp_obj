d39b161f25ba7e3626efa6396f2cc20a59db43a0
Kara Capozzi <karacapozzi@Karas-MacBook.local>
2017-09-22 14:37:31 -0400

    add methods for TDD

---
+++ temperature.rb
@@ -0,0 +1,32 @@
+
+def initialize_celsius
+
+end
+
+def initialize_fahrenheit
+
+end
+
+def convert_fahrenheit_at_freezing
+
+end
+
+def convert_celsius_at_freezing
+
+end
+
+def convert_fahrenheit_at_boiling
+
+end
+
+def convert_celsius_at_boiling
+
+end
+
+def convert_celsius_at_any_temperature
+
+end
+
+def convert_celsius_at_body_temperature
+
+end
